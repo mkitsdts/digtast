02c9934eb0617b0727418fe5813458523e289879
mkitsdts <mkitsdts@outlook.com>
2026-04-24 17:04:13 +0800

    fix: outer package could not register channel

--- internal/gateway/geteway.go
+++ internal/gateway/geteway.go
@@ -2,6 +2,7 @@ package gateway
 
 type ChannelGateway struct {
 	channels map[string]func(kind string) MessageChannel
+	alives   []MessageChannel
 }
 
 var register = make(map[string]func(kind string) MessageChannel)
@@ -13,11 +14,21 @@ func (cg *ChannelGateway) NewChannel(kind string) MessageChannel {
 	return cg.channels[kind](kind)
 }
 
+func (cg *ChannelGateway) GatewayRunningState() bool {
+	// TODO
+	for _, mc := range cg.alives {
+		if mc.IsActive() {
+			return true
+		}
+	}
+	return false
+}
+
 func NewChannelGeteway() *ChannelGateway {
 	return channelGateway
 }
 
 // 提供通道实现注册
-func registerChannel(kind string, f func(kind string) MessageChannel) {
+func RegisterChannel(kind string, f func(kind string) MessageChannel) {
 	register[kind] = f
 }
