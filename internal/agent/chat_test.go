d86f358489fd34896dc1501d42e623c1304f2036
mkitsdts <mkitsdts@outlook.com>
2026-04-27 22:40:20 +0800

    chore: add chat test

---
+++ internal/agent/chat_test.go
@@ -0,0 +1,32 @@
+package agent
+
+import (
+	"context"
+	"digital-labor/pkg/model"
+	"testing"
+)
+
+func TestChat(t *testing.T) {
+	agent, err := NewDigitalAgent(&model.DigitalAgentConfig{
+		Provider:    "doubao",
+		Key:         ark_api_key,
+		URL:         "https://ark.cn-beijing.volces.com/api/v3",
+		Model:       "doubao-seed-2-0-lite-260215",
+		Name:        "114514",
+		Description: "114514",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "session_id", "114514")
+
+	ch, err := agent.Run(ctx, "帮我查一下广州的天气", true, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for result := range ch {
+		t.Log(result)
+	}
+}
