5ccd0a6c6b51a26d73fad49b98426e396ff2cfa2
mkitsdts <mkitsdts@outlook.com>
2026-04-28 23:00:21 +0800

    chore: allow only starting ftp

--- internal/server/api/service.go
+++ internal/server/api/service.go
@@ -3,9 +3,15 @@ package api
 import (
 	"context"
 	"digital-labor/internal/center"
+	"digital-labor/pkg/conf"
 	"digital-labor/pkg/model"
 	pb "digital-labor/proto"
+	"errors"
 	"log/slog"
+)
+
+const (
+	default_ftp_port int = 2121
 )
 
 // StartService 启动服务
@@ -23,11 +29,15 @@ func (s *ContainerServer) StartService(ctx context.Context, req *pb.StartService
 	})
 	if err != nil {
 		slog.Error("Failed to create agent", "error", err)
-		return nil, err
 	}
 
-	ftp_port := 2121
+	ftp_port := -1
 	if req.FtpEnabled {
+		if conf.Conf.FTP.Port != 0 {
+			ftp_port = conf.Conf.FTP.Port
+		} else {
+			ftp_port = default_ftp_port
+		}
 		err := center.GetCenter().StartFTPServer(&center.FTPServerParams{
 			Port: ftp_port,
 		})
@@ -44,16 +54,14 @@ func (s *ContainerServer) StartService(ctx context.Context, req *pb.StartService
 		})
 		if err != nil {
 			slog.Error("Failed to start VNC server", "error", err)
-			return nil, err
 		}
 	}
 
-	slog.Info("Agent created successfully", "container_id", req.ContainerId, "agent_id", req.AgentId)
 	return &pb.StartServiceResponse{
 		VisualDisplayPort: int32(port),
 		FtpPort:           int32(ftp_port),
-		Success:           true,
-	}, nil
+		Success:           err == nil,
+	}, err
 }
 
 // StopService 暂停服务
@@ -61,53 +69,30 @@ func (s *ContainerServer) StopService(ctx context.Context, req *pb.StopServiceRe
 	slog.Info("StopService request received", "container_id", req.ContainerId, "agent_id", req.AgentId)
 	id := buildAgentKey(req.ContainerId, req.AgentId)
 
+	var errs error
 	// Stop VNC if it was running
-	_ = center.GetCenter().StopVDisplay(&center.VDisplayParams{
+	if err := center.GetCenter().StopVDisplay(&center.VDisplayParams{
 		Key: id,
-	})
-
-	// Stop FTP Server (global)
-	err := center.GetCenter().StopFTPServer()
-
-	return &pb.StopServiceResponse{
-		Success: err == nil,
-	}, err
-}
-
-// RestartService 重启服务
-func (s *ContainerServer) RestartService(ctx context.Context, req *pb.RestartServiceRequest) (*pb.RestartServiceResponse, error) {
-	// TODO: 实现实现 FTP 和 远程桌面的重启
-	return &pb.RestartServiceResponse{
-		Success: true,
-	}, nil
-}
-
-// RemoveService 移除服务
-func (s *ContainerServer) RemoveService(ctx context.Context, req *pb.RemoveServiceRequest) (*pb.RemoveServiceResponse, error) {
-	slog.Info("RemoveService request received", "container_id", req.ContainerId, "agent_id", req.AgentId)
-
-	id := buildAgentKey(req.ContainerId, req.AgentId)
-
-	// Stop VNC first
-	_ = center.GetCenter().StopVDisplay(&center.VDisplayParams{
-		Key: id,
-	})
-
-	err := center.GetCenter().RemoveAgent(id)
-	if err != nil {
-		slog.Error("Failed to remove agent", "error", err)
-		return nil, err
+	}); err != nil {
+		slog.Error("Failed to stop visual display")
+		errs = errors.New(err.Error())
 	}
 
-	slog.Info("Agent removed successfully", "container_id", req.ContainerId, "agent_id", req.AgentId)
-	return &pb.RemoveServiceResponse{
-		Success: true,
-	}, nil
+	// Stop FTP Server (global)
+	if err := center.GetCenter().StopFTPServer(); err != nil {
+		slog.Error("Failed to stop FTP server", "error", err)
+		errs = errors.New(errs.Error() + err.Error())
+	}
+
+	return &pb.StopServiceResponse{
+		Success: errs == nil,
+	}, errs
 }
 
 // BackupService 备份服务
 func (s *ContainerServer) BackupService(ctx context.Context, req *pb.BackupServiceRequest) (*pb.BackupServiceResponse, error) {
-	// TODO: 实现备份容器逻辑
+	
+	
 	return &pb.BackupServiceResponse{
 		BackupUrl: "http://backup-server/container-" + req.ContainerId + "/agent-" + req.AgentId + ".tar.gz",
 	}, nil
