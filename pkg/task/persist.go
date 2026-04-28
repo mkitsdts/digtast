b809b86a6501326140c56425cd7b68adbb66b3db
mkitsdts <mkitsdts@outlook.com>
2026-04-28 11:58:42 +0800

    chore: add task persist goruntine

--- pkg/task/persist.go
+++ pkg/task/persist.go
@@ -6,15 +6,21 @@ import (
 )
 
 func persist() {
-	if !globalTaskManager.is_dirty {
-		return
-	}
+	for {
+		if !globalTaskManager.is_dirty {
+			return
+		}
 
-	data, err := json.Marshal(globalTaskManager.Tasks)
-	if err != nil {
-		return
-	}
+		data, err := json.Marshal(globalTaskManager.Tasks)
+		if err != nil {
+			return
+		}
 
-	go workspace.PersistFile("tasks.json", data)
-	globalTaskManager.is_dirty = false
+		go workspace.PersistFile("tasks.json", data)
+		globalTaskManager.is_dirty = false
+	}
+}
+
+func init() {
+	go persist()
 }
