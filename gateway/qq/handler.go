7e04f287804a4a94b71e9506d97b89e9ef857254
mkitsdts <mkitsdts@outlook.com>
2026-05-09 19:12:01 +0800

    feat: support qq multi content parse

--- gateway/qq/handler.go
+++ gateway/qq/handler.go
@@ -9,14 +9,25 @@ import (
 	"net/http"
 )
 
+type Attachment struct {
+	ContentType  string `json:"content_type"`
+	FileName     string `json:"filename"`
+	Height       int    `json:"height"`
+	Width        int    `json:"width"`
+	Size         int    `json:"size"`
+	Url          string `json:"url"`
+	VoiceWavUrl  string `json:"voice_wav_url"`
+	AsrReferText string `json:"asr_refer_text"`
+}
+
 type C2CMessage struct {
 	ID     string `json:"id"`
 	Author struct {
 		UserOpenID string `json:"user_openid"`
 	} `json:"author"`
-	Content     string `json:"content"`
-	TimeStamp   string `json:"timestamp"`
-	Attachments []any  `json:"attachments"` // 富消息文本
+	Content     string       `json:"content"`
+	TimeStamp   string       `json:"timestamp"`
+	Attachments []Attachment `json:"attachments"` // 富消息文本
 }
 
 // C2CMessageEventHandler 实现处理 at 消息的回调
@@ -28,11 +39,28 @@ func (c *QQChannel) C2CMessageEventHandler() func(msg json.RawMessage) error {
 		}
 		slog.Debug("message received", "content", data.Content)
 
+		var multi map[string][]byte
+		if len(data.Attachments) > 0 {
+			multi = make(map[string][]byte)
+			for i := range data.Attachments {
+				url := data.Attachments[i].Url
+				if data.Attachments[i].ContentType == "voice" && data.Attachments[i].VoiceWavUrl != "" {
+					url = data.Attachments[i].VoiceWavUrl
+				}
+				body, err := c.GetMultiContent(url)
+				if err != nil {
+					continue
+				}
+				multi[data.Attachments[i].FileName] = body
+			}
+		}
+
 		content := data.Content
 		gateway.PushUserMessage(&gateway.UserMessage{
-			Content:   content,
-			NotifyWay: "qq",
-			Params:    map[string]any{"user_id": data.Author.UserOpenID, "msg_id": data.ID},
+			Content:      content,
+			NotifyWay:    "qq",
+			Params:       map[string]any{"user_id": data.Author.UserOpenID, "msg_id": data.ID},
+			MultiContent: multi,
 		})
 		slog.Info("message pushed", "content", content, "user_id", data.Author.UserOpenID, "msg_id", data.ID)
 
@@ -68,4 +96,30 @@ func (c *QQChannel) SendMessage(content, user_id, msg_id string) error {
 		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
 	return nil
+}
+
+func (c *QQChannel) GetMultiContent(url string) ([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", c.token())
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		slog.Error("get multi content failed", "status", resp.StatusCode)
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	body := make([]byte, 0)
+	if _, err = resp.Body.Read(body); err != nil {
+		return nil, err
+	}
+	return body, nil
 }
